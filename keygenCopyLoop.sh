6a74fd87287e39dfbaba096b7207540e14b9f870
mrcloudchase <67341715+mrcloudchase@users.noreply.github.com>
2020-12-18 13:56:33 -0600

    Create keygenCopyLoop.sh

---
+++ keygenCopyLoop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# IP Array for my Playground VMs
+IP_LIST=("172.31.40.140" "172.31.43.199" "172.31.47.3")
+
+# Username Variable
+USER="cloud_user"
+
+ssh-keygen
+
+# For every IP in IP_LIST array loop through them.
+for IP in ${IP_LIST[@]}; do
+    #echo $IP # Test loop
+
+# If statement to check for the iteration of our local IP
+    if [ "$IP" == `hostname -I` ]; then
+        echo "This is our IP: $IP" # Exclude our IP from the iteration and continue to next iteration
+        continue
+    fi
+        ssh-copy-id "$USER"@"$IP" # Copy ssh-key for each IP iteration
+done
